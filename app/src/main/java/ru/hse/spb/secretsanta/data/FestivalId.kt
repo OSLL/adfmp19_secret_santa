6555da8592e58d3316fa73cd87601b812a9f782c
Ivan Veselov <veselov.iva@gmail.com>
2019-02-15 15:06:12 +0300

    Add inline modifier where possible.

--- app/src/main/java/ru/hse/spb/secretsanta/data/FestivalId.kt
+++ app/src/main/java/ru/hse/spb/secretsanta/data/FestivalId.kt
@@ -3,4 +3,4 @@ package ru.hse.spb.secretsanta.data
 /**
  * Objects of this class allow identification of a festival. Helps to distinguish one festival from another.
  */
-data class FestivalId(val id: String)
+inline class FestivalId(val id: String)
--- app/src/main/java/ru/hse/spb/secretsanta/data/PersonId.kt
+++ app/src/main/java/ru/hse/spb/secretsanta/data/PersonId.kt
@@ -3,4 +3,4 @@ package ru.hse.spb.secretsanta.data
 /**
  * Objects of this class allow identification of a person. Helps to distinguish one person from another.
  */
-data class PersonId(val id: String)
+inline class PersonId(val id: String)
